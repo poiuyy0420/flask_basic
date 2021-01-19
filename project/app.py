f2882dbec0df16cffd2128bf9ba9cdfe0e932cc0
poiuyy0420 <poiuyy0420@gmail.com>
2021-01-19 19:09:32 +0900

    login / logout

--- project/app.py
+++ project/app.py
@@ -1,16 +1,32 @@
 import os
 from flask import Flask
 from flask import request
+from flask import session
 from flask import redirect
 from flask import render_template
 from models import db
 from flask_wtf.csrf import CSRFProtect
-from forms import RegisterForm
+from forms import RegisterForm, LoginForm
 
 from models import Fcuser
 
 
 app = Flask(__name__)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('userid', None)
+    return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['userid'] = form.data.get('userid')
+        return redirect('/')
+
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -32,7 +48,8 @@ def register():
 
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    userid = session.get('userid', None)
+    return render_template('hello.html', userid=userid)
 
 if __name__ == "__main__":
     basedir = os.path.abspath(os.path.dirname(__file__))
